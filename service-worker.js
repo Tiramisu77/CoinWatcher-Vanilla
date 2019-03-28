6f16a952b512f3bb3305e4dff83b0f8edbf16e58
Tiramisu77 <deadwing66@gmail.com>
2019-03-28 14:59:05 +0200

    deploy

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("workbox-v4.1.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "workbox-v4.1.1"});
 
 importScripts(
-  "precache-manifest.59b3cd0e139ddb7f67492c78d22f4c7c.js"
+  "precache-manifest.d7a9a42d948bb6b247dfc903424ede64.js"
 );
 
 workbox.core.skipWaiting();
@@ -28,5 +28,7 @@ workbox.core.skipWaiting();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerRoute(/https:\/\/api.coingecko.com\/api\/v3\/coins\/list|https:\/\/api.coingecko.com\/api\/v3\/simple\/supported_vs_currencies/, new workbox.strategies.CacheFirst({ "cacheName":"coingecko-v3", plugins: [new workbox.expiration.Plugin({ maxAgeSeconds: 518400, purgeOnQuotaError: false }), new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
-workbox.routing.registerRoute(/(https:\/\/api.coingecko.com\/api\/v3\/coins\/)(?!list)/, new workbox.strategies.NetworkFirst({ "cacheName":"coingecko-v3", plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));
+
+workbox.routing.registerRoute(/https:\/\/api.coingecko.com\/api\/v3\/coins\/list|https:\/\/api.coingecko.com\/api\/v3\/simple\/supported_vs_currencies/, new workbox.strategies.CacheFirst({ "cacheName":"coinwatcher-coingecko-v3-lists", plugins: [new workbox.expiration.Plugin({ maxAgeSeconds: 518400, purgeOnQuotaError: false }), new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
+workbox.routing.registerRoute(/(https:\/\/api.coingecko.com\/api\/v3\/coins\/)(?!list)/, new workbox.strategies.NetworkFirst({ "cacheName":"coinwatcher-coingecko-v3-coins", plugins: [new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
