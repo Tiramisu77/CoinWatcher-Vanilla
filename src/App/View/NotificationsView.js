90a933318b73d4deccb8e170d8a97eccdadf7846
Tiramisu77 <deadwing66@gmail.com>
2019-04-12 06:43:40 +0300

    quick notification fix

--- src/App/View/NotificationsView.js
+++ src/App/View/NotificationsView.js
@@ -13,34 +13,44 @@ export class NotificationsView {
     }
 
     renderPriceNotification(priceNotif, data) {
-        const lang = navigator.languages ? navigator.languages[0] : navigator.language ? navigator.language : "en-US"
-        let notification = new Notification(`Coin Watcher alert`, {
-            body: `${data.name} @ ${window.lib.numToFormattedString(data.price, {
-                type: "currency",
-                currency: priceNotif.currency,
-                lang,
-            })}`,
-            icon: priceNotif.targetIsHigher ? "/CoinWatcher/images/green-delta.png" : "/images/red-delta.png",
-        })
-        notification.onclick = () => {
-            window.focus()
-            notification.close()
+        try {
+            const lang = navigator.languages
+                ? navigator.languages[0]
+                : navigator.language
+                ? navigator.language
+                : "en-US"
+            navigator.serviceWorker.ready.then(function(registration) {
+                registration.showNotification(`Coin Watcher alert`, {
+                    body: `${data.name} @ ${window.lib.numToFormattedString(data.price, {
+                        type: "currency",
+                        currency: priceNotif.currency,
+                        lang,
+                    })}`,
+                    icon: priceNotif.targetIsHigher ? "/CoinWatcher/images/green-delta.png" : "/images/red-delta.png",
+                    requireInteraction: true,
+                })
+            })
+        } catch (e) {
+            console.error(e)
         }
     }
 
     renderPercNotification(percNotif, data) {
-        let notification = new Notification(`Coin Watcher alert`, {
-            body: `${data.name} ${percNotif.period} change: ${
-                window.lib.numToFormattedString(data.priceChange, {
-                    type: "percentage",
-                    isChange: true,
-                }).str
-            }`,
-            icon: data.priceChange < 0 ? "/CoinWatcher/images/red-delta.png" : "/images/green-delta.png",
-        })
-        notification.onclick = () => {
-            window.focus()
-            notification.close()
+        try {
+            navigator.serviceWorker.ready.then(function(registration) {
+                registration.showNotification(`Coin Watcher alert`, {
+                    body: `${data.name} ${percNotif.period} change: ${
+                        window.lib.numToFormattedString(data.priceChange, {
+                            type: "percentage",
+                            isChange: true,
+                        }).str
+                    }`,
+                    icon: data.priceChange < 0 ? "/CoinWatcher/images/red-delta.png" : "/images/green-delta.png",
+                    requireInteraction: true,
+                })
+            })
+        } catch (e) {
+            console.error(e)
         }
     }
 }
